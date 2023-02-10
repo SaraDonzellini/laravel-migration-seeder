4bf135669e4d4cdef338b361a620a3c2137f61be
SaraDonzellini <saradonzie@gmail.com>
2023-02-10 17:31:20 +0100

    corrected date format

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
+use DateTime;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    public function index() {
-        $trains = Train::where('orario_di_partenza', "like", '2023-02-10%')->get();
+    public function index()
+    {
+        $today = new DateTime();
+        $trains = Train::where('orario_di_partenza', 'like', $today->format('Y-m-d') . '%')->get();
         return view('home', compact('trains'));
-    } 
+    }
 }
